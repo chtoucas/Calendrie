301edeb431cc425f4f0d7a643b8734983bc75cb3
chtoucas <policromia@gmail.com>
2024-11-28 06:26:34 +0100

    Update IBlankDayFeaturette.cs

--- src/Calendrie/Core/IBlankDayFeaturette.cs
+++ src/Calendrie/Core/IBlankDayFeaturette.cs
@@ -13,15 +13,20 @@ namespace Calendrie.Core;
 // community"; see the wikipedia page
 // https://en.wikipedia.org/wiki/Hugh_Jones_(professor)
 
-/// <summary>Defines methods specific to calendrical schemas featuring blank days.
-/// <para>A blank-day schema is a solar schema that adds one extra blank day on common years and two
-/// on leap years. A blank day does not belong to any month and is kept outside the weekday cycle.
+/// <summary>
+/// Defines methods specific to calendrical schemas featuring blank days.
+/// <para>A blank-day schema is a solar schema that adds one extra blank day on
+/// common years and two on leap years. A blank day does not belong to any month
+/// and is kept outside the weekday cycle.</para>
+/// <para>For technical reasons, we pretend that a blank day is the last day of
+/// the preceding month.</para>
+/// <para>Blank-day calendars belong to the larger family of perennial calendars.
 /// </para>
-/// <para>For technical reasons, we pretend that a blank day is the last day of the preceding month.
-/// </para>
-/// <para>Blank-day calendars belong to the larger family of perennial calendars.</para></summary>
+/// </summary>
 public interface IBlankDayFeaturette : ICalendricalKernel
 {
-    /// <summary>Determines whether the specified date is a blank day or not.</summary>
+    /// <summary>
+    /// Determines whether the specified date is a blank day or not.
+    /// </summary>
     [Pure] bool IsBlankDay(int y, int m, int d);
 }
