563c7660db71c19a95b192d973b0bfb7950f9aaa
chtoucas <policromia@gmail.com>
2024-11-29 07:36:57 +0100

    Benchmark the most important schemas

---
+++ src/Calendrie.Benchmarks/Tests/GregorianSchemaTests.cs
@@ -0,0 +1,138 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Benchmarks.Tests;
+
+using Calendrie.Core.Schemas;
+
+using static Benchmarks.BenchmarkHelpers;
+
+public abstract class GregorianSchemaTests
+{
+    private protected GregorianSchema schema = new();
+    private protected CivilSchema civilSchema = new();
+
+    private protected int year, month, day, daysSinceEpoch;
+    private protected long yearL, daysSinceEpochL;
+
+    private protected GregorianSchemaTests(GJDateType type = GJDateType.FixedFast)
+    {
+        (year, month, day) = CreateGregorianParts(type);
+        daysSinceEpoch = DayNumber.FromGregorianParts(year, month, day).DaysSinceZero;
+        (yearL, daysSinceEpochL) = (year, daysSinceEpoch);
+    }
+}
+
+public class GregorianSchema_CountDaysInMonth : GregorianSchemaTests
+{
+    [Benchmark(Baseline = true)]
+    public int Formulae() => GregorianFormulae.CountDaysInMonth(year, month);
+
+    [Benchmark]
+    public int Formulae64() => GregorianFormulae.CountDaysInMonth(yearL, month);
+
+    [Benchmark]
+    public int Schema() => schema.CountDaysInMonth(year, month);
+}
+
+public class GregorianSchema_CountDaysSinceEpoch : GregorianSchemaTests
+{
+    [Benchmark]
+    public int Formulae() => GregorianFormulae.CountDaysSinceEpoch(year, month, day);
+
+    [Benchmark(Baseline = true)]
+    public int Formulae_Civil() => CivilFormulae.CountDaysSinceEpoch(year, month, day);
+
+    [Benchmark]
+    public long Formulae64() => GregorianFormulae.CountDaysSinceEpoch(yearL, month, day);
+
+    [Benchmark]
+    public int Schema() => schema.CountDaysSinceEpoch(year, month, day);
+
+    [Benchmark]
+    public int Schema_Civil() => civilSchema.CountDaysSinceEpoch(year, month, day);
+}
+
+public class GregorianSchema_GetDateParts : GregorianSchemaTests
+{
+    [Benchmark]
+    public int Formulae()
+    {
+        GregorianFormulae.GetDateParts(daysSinceEpoch, out int y, out int m, out int d);
+        Consume(in y);
+        Consume(in m);
+        return d;
+    }
+
+    [Benchmark(Baseline = true)]
+    public int Formulae_Civil()
+    {
+        CivilFormulae.GetDateParts(daysSinceEpoch, out int y, out int m, out int d);
+        Consume(in y);
+        Consume(in m);
+        return d;
+    }
+
+    [Benchmark]
+    public int Formulae64()
+    {
+        GregorianFormulae.GetDateParts(daysSinceEpoch, out long y, out int m, out int d);
+        Consume(in y);
+        Consume(in m);
+        return d;
+    }
+
+    [Benchmark]
+    public int Schema()
+    {
+        schema.GetDateParts(daysSinceEpoch, out int y, out int m, out int d);
+        Consume(in y);
+        Consume(in m);
+        return d;
+    }
+
+    [Benchmark]
+    public int Schema_Civil()
+    {
+        civilSchema.GetDateParts(daysSinceEpoch, out int y, out int m, out int d);
+        Consume(in y);
+        Consume(in m);
+        return d;
+    }
+}
+
+public class GregorianSchema_GetYear : GregorianSchemaTests
+{
+    [Benchmark]
+    public int Formulae() => GregorianFormulae.GetYear(daysSinceEpoch);
+
+    [Benchmark(Baseline = true)]
+    public int Formulae_Civil() => CivilFormulae.GetYear(daysSinceEpoch);
+
+    [Benchmark]
+    public long Formulae64() => GregorianFormulae.GetYear(daysSinceEpochL);
+
+    [Benchmark]
+    public int Schema() => schema.GetYear(daysSinceEpoch);
+
+    [Benchmark]
+    public int Schema_Civil() => civilSchema.GetYear(daysSinceEpoch);
+}
+
+public class GregorianSchema_GetStartOfYear : GregorianSchemaTests
+{
+    [Benchmark]
+    public int Formulae() => GregorianFormulae.GetStartOfYear(year);
+
+    [Benchmark(Baseline = true)]
+    public int Formulae_Civil() => CivilFormulae.GetStartOfYear(year);
+
+    [Benchmark]
+    public long Formulae64() => GregorianFormulae.GetStartOfYear(yearL);
+
+    [Benchmark]
+    public int Schema() => schema.GetStartOfYear(year);
+
+    [Benchmark]
+    public int Schema_Civil() => civilSchema.GetStartOfYear(year);
+}
--- src/Calendrie/Core/Schemas/CivilSchema.cs
+++ src/Calendrie/Core/Schemas/CivilSchema.cs
@@ -54,30 +54,6 @@ public partial class CivilSchema // Conversions
     /// <inheritdoc />
     public sealed override void GetDateParts(int daysSinceEpoch, out int y, out int m, out int d)
     {
-#if false
-        uint daysSinceEpoch1 = (uint)daysSinceEpoch + DaysInYearAfterFebruary;
-
-        uint C = ((daysSinceEpoch1 << 2) + 3) / GregorianSchema.DaysPer400YearCycle;
-        uint D = daysSinceEpoch1 - (GregorianSchema.DaysPer400YearCycle * C >> 2);
-
-        uint Y = ((D << 2) + 3) / GregorianSchema.DaysPer4YearSubcycle;
-        uint d0y = D - (GregorianSchema.DaysPer4YearSubcycle * Y >> 2);
-
-        m = (int)((5 * d0y + 2) / 153);
-        d = (int)(1 + d0y - (uint)(153 * m + 2) / 5);
-
-        if (m > 9)
-        {
-            Y++;
-            m -= 9;
-        }
-        else
-        {
-            m += 3;
-        }
-
-        y = (int)(100 * C + Y);
-#else
         daysSinceEpoch += DaysInYearAfterFebruary;
 
         int C = (int)((uint)((daysSinceEpoch << 2) + 3) / GregorianSchema.DaysPer400YearCycle);
@@ -100,7 +76,6 @@ public partial class CivilSchema // Conversions
         }
 
         y = 100 * C + Y;
-#endif
     }
 
     /// <inheritdoc />
