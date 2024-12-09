70e8f27ffa5b067103f7e6d6c0aa803a04a90e3b
Chtoucas <policromia@gmail.com>
2024-12-09 18:17:45 +0100

    Add new tests for RangeExtensions.

---
+++ src/Calendrie.Tests/Tests/Core/Validation/RangeExtensionsTests.fs
@@ -0,0 +1,25 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+module Calendrie.Tests.Core.Validation.RangeExtensionsTests
+
+open Calendrie.Core.Intervals
+open Calendrie.Core.Validation
+open Calendrie.Testing
+
+open Xunit
+
+// TODO(code): pourquoi ça ne marche en tant que méthodes d'extension ?
+
+[<Fact>]
+let ``Validate()`` () =
+    let range : Range<int> = Range.Create(0, 2)
+
+    outOfRangeExn "paramName" (fun () -> RangeExtensions.Validate(range, -1, "paramName"))
+    outOfRangeExn "value" (fun () -> RangeExtensions.Validate(range, -1))
+    RangeExtensions.Validate(range, 0)
+    RangeExtensions.Validate(range, 1)
+    RangeExtensions.Validate(range, 2)
+    outOfRangeExn "value" (fun () -> RangeExtensions.Validate(range, 3))
+    outOfRangeExn "paramName" (fun () -> RangeExtensions.Validate(range, 3, "paramName"))
+
