8fce98b0bd0824dad9334f7458c1e177fd869de1
chtoucas <policromia@gmail.com>
2024-12-10 09:08:35 +0100

    Remove legacy files

--- src/Calendrie.Sketches/Usings.cs
+++ src/Calendrie.Sketches/Usings.cs
@@ -2,7 +2,6 @@
 // Copyright (c) Tran Ngoc Bich. All rights reserved.
 
 global using System;
-global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Diagnostics.Contracts;
