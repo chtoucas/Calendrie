bf5c4ac63c730ae182a75d51007e7d8bf170f902
chtoucas <policromia@gmail.com>
2024-12-11 06:44:26 +0100

    Update UnboundedTests.fs

--- src/Calendrie.Tests/Tests/Core/Intervals/UnboundedTests.fs
+++ src/Calendrie.Tests/Tests/Core/Intervals/UnboundedTests.fs
@@ -47,3 +47,86 @@ module SetOperations =
     [<Property>]
     let ``Contains() always returns true`` (i: int) =
         Unbounded<int>.Instance.Contains(i) |> ok
+
+    //
+    // Set inclusion
+    //
+
+    [<Fact>]
+    let ``Set inclusion throws for null`` () =
+        let x = Unbounded<int>.Instance
+
+        nullExn "other" (fun () -> x.IsSubsetOf(null))
+        //nullExn "other" (fun () -> x.IsSupersetOf(null))
+        //nullExn "other" (fun () -> x.IsProperSubsetOf(null))
+        nullExn "other" (fun () -> x.IsProperSupersetOf(null))
+
+    [<Fact>]
+    let ``Set inclusion with itsef`` () =
+        let x = Unbounded<int>.Instance
+
+        x.IsSubsetOf(x)         |> ok
+        //x.IsSupersetOf(x)       |> ok
+        //x.IsProperSubsetOf(x)   |> nok
+        x.IsProperSupersetOf(x) |> nok
+
+    [<Property>]
+    let ``Set inclusion with a lower ray`` (y: LowerRay<int>) =
+        let x = Unbounded<int>.Instance
+
+        x.IsSubsetOf(y)         |> nok
+        //x.IsSupersetOf(y)       |> ok
+        //x.IsProperSubsetOf(y)   |> nok
+        x.IsProperSupersetOf(y) |> ok
+
+    [<Property>]
+    let ``Set inclusion with an upper ray`` (y: UpperRay<int>) =
+        let x = Unbounded<int>.Instance
+
+        x.IsSubsetOf(y)         |> nok
+        //x.IsSupersetOf(y)       |> ok
+        //x.IsProperSubsetOf(y)   |> nok
+        x.IsProperSupersetOf(y) |> ok
+
+    [<Property>]
+    let ``Set inclusion with a range`` (y: Range<int>) =
+        let x = Unbounded<int>.Instance
+
+        x.IsSubsetOf(y)         |> nok
+        //x.IsSupersetOf(y)       |> ok
+        //x.IsProperSubsetOf(y)   |> nok
+        x.IsProperSupersetOf(y) |> ok
+
+    //
+    // Set equality
+    //
+
+    [<Fact>]
+    let ``SetEquals() throws for null`` () =
+        let x = Unbounded<int>.Instance
+
+        nullExn "other" (fun () -> x.SetEquals(null))
+
+    [<Property>]
+    let ``SetEquals() with itself`` () =
+        let x = Unbounded<int>.Instance
+
+        x.SetEquals(x)
+
+    [<Property>]
+    let ``SetEquals() with a lower ray`` (y: LowerRay<int>) =
+        let x = Unbounded<int>.Instance
+
+        not (x.SetEquals(y))
+
+    [<Property>]
+    let ``SetEquals() with an upper ray`` (y: UpperRay<int>) =
+        let x = Unbounded<int>.Instance
+
+        not (x.SetEquals(y))
+
+    [<Property>]
+    let ``SetEquals() with a range`` (y: Range<int>) =
+        let x = Unbounded<int>.Instance
+
+        not (x.SetEquals(y))
