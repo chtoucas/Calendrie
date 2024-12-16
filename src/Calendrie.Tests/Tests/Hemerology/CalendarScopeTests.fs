4d83dc928ec583631dd7fd74724070e08642227e
Chtoucas <policromia@gmail.com>
2024-12-16 16:11:52 +0100

    Move tests

--- src/Calendrie.Tests/Tests/Hemerology/CalendarScopeTests.fs
+++ src/Calendrie.Tests/Tests/Hemerology/CalendarScopeTests.fs
@@ -46,3 +46,46 @@ module Prelude =
         scope.Segment ==& seg
         // It's enough to check the property Range.
         scope.YearsValidator.Range === seg.SupportedYears
+
+module DayNumberValidation =
+    [<Fact>]
+    let ``Validate()`` () =
+        let range = Range.Create(DayNumber.Zero, DayNumber.Zero + 2)
+
+        //outOfRangeExn "paramName" (fun () -> range.Validate(DayNumber.Zero - 1, "paramName"))
+        outOfRangeExn "dayNumber" (fun () -> range.Validate(DayNumber.Zero - 1))
+        range.Validate(DayNumber.Zero)
+        range.Validate(DayNumber.Zero + 1)
+        range.Validate(DayNumber.Zero + 2)
+        outOfRangeExn "dayNumber" (fun () -> range.Validate(DayNumber.Zero + 3))
+        //outOfRangeExn "paramName" (fun () -> range.Validate(DayNumber.Zero + 3, "paramName"))
+
+    [<Fact>]
+    let ``CheckOverflow()`` () =
+        let range = Range.Create(DayNumber.Zero, DayNumber.Zero + 2)
+
+        (fun () -> range.CheckOverflow(DayNumber.Zero - 1)) |> overflows
+        range.CheckOverflow(DayNumber.Zero)
+        range.CheckOverflow(DayNumber.Zero + 1)
+        range.CheckOverflow(DayNumber.Zero + 2)
+        (fun () -> range.CheckOverflow(DayNumber.Zero + 3)) |> overflows
+
+    [<Fact>]
+    let ``CheckUpperBound()`` () =
+        let range = Range.Create(DayNumber.Zero, DayNumber.Zero + 2)
+
+        range.CheckUpperBound(DayNumber.Zero - 1)
+        range.CheckUpperBound(DayNumber.Zero)
+        range.CheckUpperBound(DayNumber.Zero + 1)
+        range.CheckUpperBound(DayNumber.Zero + 2)
+        (fun () -> range.CheckUpperBound(DayNumber.Zero + 3)) |> overflows
+
+    [<Fact>]
+    let ``CheckLowerBound()`` () =
+        let range = Range.Create(DayNumber.Zero, DayNumber.Zero + 2)
+
+        (fun () -> range.CheckLowerBound(DayNumber.Zero - 1)) |> overflows
+        range.CheckLowerBound(DayNumber.Zero)
+        range.CheckLowerBound(DayNumber.Zero + 1)
+        range.CheckLowerBound(DayNumber.Zero + 2)
+        range.CheckLowerBound(DayNumber.Zero + 3)
