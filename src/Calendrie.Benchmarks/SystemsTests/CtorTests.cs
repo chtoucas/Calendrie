dbfbcf0f8b073ac6728d5798654fca6172f1ec7b
chtoucas <policromia@gmail.com>
2024-12-17 06:44:39 +0100

    Update CtorTests.cs

--- src/Calendrie.Benchmarks/SystemsTests/CtorTests.cs
+++ src/Calendrie.Benchmarks/SystemsTests/CtorTests.cs
@@ -7,47 +7,48 @@ using Calendrie.Systems;
 
 public class CtorTests
 {
-    private const int
-        Year = 2017,
-        Month = 11,
-        Day = 19;
+    private int _year, _month, _day;
+
+    [GlobalSetup]
+    public void GlobalSetup() =>
+        (_year, _month, _day) = BenchmarkHelpers.CreateJulianParts();
 
     [Benchmark(Description = "ArmenianDate")]
-    public ArmenianDate WithArmenianDate() => new(Year, Month, Day);
+    public ArmenianDate WithArmenianDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "Armenian13Date")]
-    public Armenian13Date WithArmenian13Date() => new(Year, Month, Day);
+    public Armenian13Date WithArmenian13Date() => new(_year, _month, _day);
 
-    [Benchmark(Description = "CivilDate")]
-    public CivilDate WithCivilDate() => new(Year, Month, Day);
+    [Benchmark(Description = "CivilDate", Baseline = true)]
+    public CivilDate WithCivilDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "CopticDate")]
-    public CopticDate WithCopticDate() => new(Year, Month, Day);
+    public CopticDate WithCopticDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "Coptic13Date")]
-    public Coptic13Date WithCoptic13Date() => new(Year, Month, Day);
+    public Coptic13Date WithCoptic13Date() => new(_year, _month, _day);
 
     [Benchmark(Description = "EthiopicDate")]
-    public EthiopicDate WithEthiopicDate() => new(Year, Month, Day);
+    public EthiopicDate WithEthiopicDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "Ethiopic13Date")]
-    public Ethiopic13Date WithEthiopic13Date() => new(Year, Month, Day);
+    public Ethiopic13Date WithEthiopic13Date() => new(_year, _month, _day);
 
     [Benchmark(Description = "GregorianDate")]
-    public GregorianDate WithGregorianDate() => new(Year, Month, Day);
+    public GregorianDate WithGregorianDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "JulianDate")]
-    public JulianDate WithJulianDate() => new(Year, Month, Day);
+    public JulianDate WithJulianDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "TabularIslamicDate")]
-    public TabularIslamicDate WithTabularIslamicDate() => new(Year, Month, Day);
+    public TabularIslamicDate WithTabularIslamicDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "WorldDate")]
-    public WorldDate WithWorldDate() => new(Year, Month, Day);
+    public WorldDate WithWorldDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "ZoroastrianDate")]
-    public ZoroastrianDate WithZoroastrianDate() => new(Year, Month, Day);
+    public ZoroastrianDate WithZoroastrianDate() => new(_year, _month, _day);
 
     [Benchmark(Description = "Zoroastrian13Date")]
-    public Zoroastrian13Date WithZoroastrian13Date() => new(Year, Month, Day);
+    public Zoroastrian13Date WithZoroastrian13Date() => new(_year, _month, _day);
 }
