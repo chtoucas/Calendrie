73b5ab6ef4f883785374a270d254fd82780ed521
chtoucas <policromia@gmail.com>
2024-12-26 05:31:56 +0100

    Improve test coverage

--- src/Calendrie.Tests/Tests/Core/Prototyping/NonRegularSchemaTestSuite.fs
+++ src/Calendrie.Tests/Tests/Core/Prototyping/NonRegularSchemaTestSuite.fs
@@ -25,11 +25,27 @@ type LunisolarTests() =
     override x.PreValidator_Prop() = x.VerifyThatPreValidatorIs<LunisolarPreValidator>()
     override x.IsRegular() = x.SchemaUT.IsRegular() === (false, 0)
 
+// We add this temporary test for full test coverage. We lie about the value of
+// MinMonthsInYear and use 12 instead of the actual value 13.
 [<Sealed>]
 [<TestPerformance(TestPerformance.SlowBundle)>]
 [<TestExcludeFrom(TestExcludeFrom.Regular)>]
-// We do not exclude this one from CodeCoverage: necessary to fully cover GetMonthParts().
+// We do not exclude this one from CodeCoverage: necessary to fully cover
+// GetMonthParts() when monthsSinceEpoch >= 0.
 //[<TestExcludeFrom(TestExcludeFrom.CodeCoverage)>]
+type PaxTmpTests() =
+    inherit NonRegularSchemaPrototypeFacts<PaxDataSet>(new PaxSchema(), 12)
+
+    override x.Algorithm_Prop() = x.SchemaUT.Algorithm === CalendricalAlgorithm.Arithmetical
+    override x.Family_Prop() = x.SchemaUT.Family === CalendricalFamily.Other
+    override x.PeriodicAdjustments_Prop() = x.SchemaUT.PeriodicAdjustments === CalendricalAdjustments.Weeks
+    override x.PreValidator_Prop() = x.VerifyThatPreValidatorIs<PlainPreValidator>()
+    override x.IsRegular() = x.SchemaUT.IsRegular() === (false, 0)
+
+[<Sealed>]
+[<TestPerformance(TestPerformance.SlowBundle)>]
+[<TestExcludeFrom(TestExcludeFrom.Regular)>]
+[<TestExcludeFrom(TestExcludeFrom.CodeCoverage)>]
 type PaxTests() =
     inherit NonRegularSchemaPrototypeFacts<PaxDataSet>(new PaxSchema())
 
