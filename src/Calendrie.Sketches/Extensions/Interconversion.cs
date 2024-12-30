5c0fa3e9dc12c869e60054e3db7ac3ff9a3c9255
chtoucas <policromia@gmail.com>
2024-12-30 05:48:24 +0100

    Create Interconversion.cs

---
+++ src/Calendrie.Sketches/Extensions/Interconversion.cs
@@ -0,0 +1,28 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Extensions;
+
+using Calendrie.Hemerology;
+
+public static class Interconversion
+{
+    /// <summary>
+    /// Converts the specified date to a <typeparamref name="TResult"/> value.
+    /// </summary>
+    /// <exception cref="ArgumentNullException"><paramref name="date"/> is
+    /// <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="date"/>
+    /// is outside the range of supported values by <typeparamref name="TResult"/>.
+    /// </exception>
+    /// <typeparam name="TResult">The type of the absolute date result.
+    /// </typeparam>
+    [Pure]
+    public static TResult ConvertTo<TResult>(this IAbsoluteDate date)
+        where TResult : IAbsoluteDate<TResult>
+    {
+        ArgumentNullException.ThrowIfNull(date);
+
+        return TResult.FromDayNumber(date.DayNumber);
+    }
+}
