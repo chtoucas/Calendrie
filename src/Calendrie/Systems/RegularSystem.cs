61849746ec2c411f17f70774fef0ae856cc0602f
chtoucas <policromia@gmail.com>
2025-01-03 08:53:01 +0100

    Math ops

--- src/Calendrie/Systems/RegularSystem.cs
+++ src/Calendrie/Systems/RegularSystem.cs
@@ -3,10 +3,8 @@
 
 namespace Calendrie.Systems;
 
-using Calendrie.Core;
 using Calendrie.Core.Utilities;
 using Calendrie.Hemerology;
-using Calendrie.Systems.Arithmetic;
 
 public partial class RegularSystem<TDate> : CalendarSystem<TDate>
     where TDate : struct, IDateable, IAbsoluteDate<TDate>, IDateFactory<TDate>
@@ -17,19 +15,14 @@ public partial class RegularSystem<TDate> : CalendarSystem<TDate>
     private readonly int _monthsInYear;
 
     /// <summary>
-    /// Represents the calendrical arithmetic.
-    /// </summary>
-    private readonly RegularArithmetic _arithmetic;
-
-    /// <summary>
     /// Represents the earliest supported year.
-    /// <para>This field is a constant equal to -999_998.</para>
+    /// <para>This field is a constant equal to 1.</para>
     /// </summary>
     private const int MinYear_ = StandardScope.MinYear;
 
     /// <summary>
     /// Represents the latest supported year.
-    /// <para>This field is a constant equal to 999_999.</para>
+    /// <para>This field is a constant equal to 9999.</para>
     /// </summary>
     private const int MaxYear_ = StandardScope.MaxYear;
 
@@ -47,16 +40,11 @@ public partial class RegularSystem<TDate> : CalendarSystem<TDate>
 
         if (!schema.IsRegular(out _monthsInYear))
             throw new ArgumentException(null, nameof(scope));
-
-        _arithmetic = schema is LimitSchema sch ? new RegularArithmetic(sch)
-            : throw new ArgumentException(null, nameof(scope));
     }
 }
 
 public partial class RegularSystem<TDate> // Non-standard math ops
 {
-    // TODO(code): overflows? document them if any?
-
     /// <summary>
     /// Adds a number of years to the year field of the specified date.
     /// </summary>
@@ -65,17 +53,20 @@ public partial class RegularSystem<TDate> // Non-standard math ops
     [Pure]
     internal TDate AddYears(TDate date, int years)
     {
-        var sch = Scope.Schema;
-        sch.GetDateParts(date.DaysSinceEpoch, out int y, out int m, out int d);
+        Scope.Schema.GetDateParts(date.DaysSinceEpoch, out int y, out int m, out int d);
+        return AddYears(y, m, d, years);
+    }
 
-        int newY = checked(y + years);
-        if (newY < MinYear_ || newY > MaxYear_) ThrowHelpers.ThrowDateOverflow();
-
-        // NB: AdditionRule.Truncate.
-        int newD = Math.Min(d, sch.CountDaysInMonth(newY, m));
-
-        int daysSinceEpoch = sch.CountDaysSinceEpoch(newY, m, newD);
-        return TDate.UnsafeCreate(daysSinceEpoch);
+    /// <summary>
+    /// Adds a number of months to the month field of the specified date.
+    /// </summary>
+    /// <exception cref="OverflowException">The calculation would overflow the
+    /// range of supported dates.</exception>
+    [Pure]
+    internal TDate AddMonths(TDate date, int months)
+    {
+        Scope.Schema.GetDateParts(date.DaysSinceEpoch, out int y, out int m, out int d);
+        return AddMonths(y, m, d, months);
     }
 
     /// <summary>
@@ -84,10 +75,14 @@ public partial class RegularSystem<TDate> // Non-standard math ops
     [Pure]
     internal int CountYearsBetween(TDate start, TDate end)
     {
-        // Exact difference between two years.
-        int years = end.Year - start.Year;
+        Scope.Schema.GetDateParts(start.DaysSinceEpoch, out int y0, out int m0, out int d0);
 
-        var newStart = AddYears(start, years);
+        // Exact difference between two calendar years.
+        int years = end.Year - y0;
+
+        // To avoid extracting y0 twice, we inline:
+        // > var newStart = other.PlusYears(years);
+        var newStart = AddYears(y0, m0, d0, years);
         if (start < end)
         {
             if (newStart > end) years--;
@@ -101,24 +96,6 @@ public partial class RegularSystem<TDate> // Non-standard math ops
     }
 
     /// <summary>
-    /// Adds a number of months to the month field of the specified date.
-    /// </summary>
-    /// <exception cref="OverflowException">The calculation would overflow the
-    /// range of supported dates.</exception>
-    [Pure]
-    internal TDate AddMonths(TDate date, int months)
-    {
-        var sch = Scope.Schema;
-        sch.GetDateParts(date.DaysSinceEpoch, out int y, out int m, out int d);
-
-        // NB: _arithmetic.AddMonths() is validating.
-        var (newY, newM, newD) = _arithmetic.AddMonths(y, m, d, months);
-
-        int daysSinceEpoch = sch.CountDaysSinceEpoch(newY, newM, newD);
-        return TDate.UnsafeCreate(daysSinceEpoch);
-    }
-
-    /// <summary>
     /// Counts the number of months between the two specified dates.
     /// </summary>
     [Pure]
@@ -128,12 +105,12 @@ public partial class RegularSystem<TDate> // Non-standard math ops
         sch.GetDateParts(start.DaysSinceEpoch, out int y0, out int m0, out int d0);
         sch.GetDateParts(end.DaysSinceEpoch, out int y1, out int m1, out _);
 
-        // Exact difference between two months.
-        int months = _arithmetic.CountMonthsBetween(new Yemo(y0, m0), new Yemo(y1, m1));
+        // Exact difference between two calendar months.
+        int months = checked(_monthsInYear * (y1 - y0) + m1 - m0);
 
         // To avoid extracting (y0, m0, d0) twice, we inline:
         // > var newStart = AddMonths(start, months);
-        var newStart = startPlusMonths(months);
+        var newStart = AddMonths(y0, m0, d0, months);
 
         if (start < end)
         {
@@ -145,23 +122,48 @@ public partial class RegularSystem<TDate> // Non-standard math ops
         }
 
         return months;
-
-        [Pure]
-        TDate startPlusMonths(int months)
-        {
-            var (newY, newM, newD) = _arithmetic.AddMonths(y0, m0, d0, months);
-            int daysSinceEpoch = sch.CountDaysSinceEpoch(newY, newM, newD);
-            return TDate.UnsafeCreate(daysSinceEpoch);
-        }
     }
 
+    /// <summary>
+    /// Adds a number of years to the year field of the specified date.
+    /// </summary>
+    /// <exception cref="OverflowException">The calculation would overflow the
+    /// range of supported dates.</exception>
     [Pure]
-    private Yemo AddMonths(int y, int m, int months)
+    private TDate AddYears(int y, int m, int d, int years)
     {
+        var sch = Scope.Schema;
+
+        // Exact addition of years to a calendar year.
+        int newY = checked(y + years);
+        if (newY < MinYear_ || newY > MaxYear_) ThrowHelpers.ThrowDateOverflow();
+
+        // NB: AdditionRule.Truncate.
+        int newD = Math.Min(d, sch.CountDaysInMonth(newY, m));
+
+        int daysSinceEpoch = sch.CountDaysSinceEpoch(newY, m, newD);
+        return TDate.UnsafeCreate(daysSinceEpoch);
+    }
+
+    /// <summary>
+    /// Adds a number of months to the month field of the specified date.
+    /// </summary>
+    /// <exception cref="OverflowException">The calculation would overflow the
+    /// range of supported dates.</exception>
+    [Pure]
+    private TDate AddMonths(int y, int m, int d, int months)
+    {
+        var sch = Scope.Schema;
+
+        // Exact addition of months to a calendar month.
         int newM = 1 + MathZ.Modulo(checked(m - 1 + months), _monthsInYear, out int y0);
         int newY = checked(y + y0);
         if (newY < MinYear_ || newY > MaxYear_) ThrowHelpers.ThrowMonthOverflow();
 
-        return new Yemo(newY, newM);
+        // NB: AdditionRule.Truncate.
+        int newD = Math.Min(d, sch.CountDaysInMonth(newY, newM));
+
+        int daysSinceEpoch = sch.CountDaysSinceEpoch(newY, newM, newD);
+        return TDate.UnsafeCreate(daysSinceEpoch);
     }
 }
