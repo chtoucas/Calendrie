6388800c8237eca59b2975b3e015f608d8a2fff5
Chtoucas <policromia@gmail.com>
2025-01-04 23:10:13 +0100

    Create ICalendarBound.cs

---
+++ src/Calendrie.Sketches/Hemerology/ICalendarBound.cs
@@ -0,0 +1,13 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Hemerology;
+
+public interface ICalendarBound<out TCalendar>
+{
+    /// <summary>
+    /// Gets the calendar to which belongs the current type.
+    /// <para>This static property is thread-safe.</para>
+    /// </summary>
+    static abstract TCalendar Calendar { get; }
+}
