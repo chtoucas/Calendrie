d8eddf82e473cf23a6cc347f83fa486ac59b3ff6
Chtoucas <policromia@gmail.com>
2025-01-08 19:43:16 +0100

    Create Civil.cs

---
+++ src/Calendrie.Future/Systems/Civil.cs
@@ -0,0 +1,33 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Systems;
+
+using Calendrie.Core.Schemas;
+
+using static Calendrie.Core.CalendricalConstants;
+
+public static class Civil
+{
+    /// <summary>
+    /// Attempts to create a new instance of the <see cref="CivilDate"/>
+    /// struct from the specified date components.
+    /// </summary>
+    [Pure]
+    public static CivilDate? TryCreate(int year, int month, int day)
+    {
+        bool ok = year >= CivilScope.MinYear && year <= CivilScope.MaxYear
+            && month >= 1 && month <= CivilCalendar.MonthsInYear
+            && day >= 1
+            && (day <= Solar.MinDaysInMonth
+                || day <= GregorianFormulae.CountDaysInMonth(year, month));
+
+        if (ok)
+        {
+            int daysSinceZero = CivilFormulae.CountDaysSinceEpoch(year, month, day);
+            return new CivilDate(daysSinceZero);
+        }
+
+        return null;
+    }
+}
