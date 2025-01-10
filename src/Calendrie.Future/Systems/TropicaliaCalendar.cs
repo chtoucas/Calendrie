f831759a94aba3d8039a4859bc0cb788760f3e0b
Chtoucas <policromia@gmail.com>
2025-01-10 18:00:01 +0100

    Delete files

--- src/Calendrie.Future/Systems/TropicaliaCalendar.cs
+++
@@ -1,8 +0,0 @@
-﻿// SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) Tran Ngoc Bich. All rights reserved.
-
-namespace Calendrie.Systems;
-
-using Calendrie.Hemerology;
-
-public partial class TropicaliaCalendar : ICalendar<TropicaliaDate> { }
