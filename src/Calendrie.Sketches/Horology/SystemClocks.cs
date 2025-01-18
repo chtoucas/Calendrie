ab2fa50a0a4c37765fb395800f53426cbc9f1a6f
Chtoucas <policromia@gmail.com>
2025-01-18 09:46:26 +0100

    Add clocks

---
+++ src/Calendrie.Sketches/Horology/SystemClocks.cs
@@ -0,0 +1,25 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Horology;
+
+/// <summary>
+/// Provides system clocks.
+/// <para>This class cannot be inherited.</para>
+/// </summary>
+public static class SystemClocks
+{
+    /// <summary>
+    /// Gets an instance of the system clock using the current time zone setting
+    /// on this machine.
+    /// <para>This static property is thread-safe.</para>
+    /// </summary>
+    public static LocalSystemClock Local => LocalSystemClock.Instance;
+
+    /// <summary>
+    /// Gets an instance of the system clock using the Coordinated Universal
+    /// Time (UTC).
+    /// <para>This static property is thread-safe.</para>
+    /// </summary>
+    public static UtcSystemClock Utc => UtcSystemClock.Instance;
+}
---
+++ src/Calendrie.Sketches/Horology/UtcSystemClock.cs
@@ -0,0 +1,72 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Horology;
+
+using Calendrie.Core;
+
+using static Calendrie.Core.TemporalConstants;
+
+// Beware, we use DateTime.Ticks but
+// > "It does not include the number of ticks that are attributable to leap seconds."
+// See
+// - https://docs.microsoft.com/en-us/dotnet/api/system.datetime.ticks
+// - https://github.com/dotnet/dotnet-api-docs/issues/966
+//
+// Consequences?
+// Regarding DateTime,
+// - In case of a positive leap second, 23:59:59 is repeated.
+// - In case of a negative leap second, 23:59:59 is not valid but I don't
+//   know how DateTime actually handles this case. Notice that no negative
+//   leap second has ever occured so far.
+//
+// Being based on the OS clock, this clock is NOT monotonic.
+// In fact, it does not matter, one SHOULD NOT use this clock for timing.
+// See also
+// - https://github.com/dotnet/runtime/issues/15207
+// - https://github.com/dotnet/runtime/issues/5883
+
+/// <summary>
+/// Represents the system clock using the Coordinated Universal Time (UTC).
+/// <para>See <see cref="SystemClocks.Utc"/>.</para>
+/// <para>This class cannot be inherited.</para>
+/// </summary>
+public sealed class UtcSystemClock : IClock
+{
+    /// <summary>
+    /// Represents a singleton instance of the <see cref="UtcSystemClock"/> class.
+    /// <para>This field is read-only.</para>
+    /// </summary>
+    internal static readonly UtcSystemClock Instance = new();
+
+    private UtcSystemClock() { }
+
+    /// <inheritdoc/>
+    [Pure]
+    public Moment Now()
+    {
+        // This method works only because DateTime.Ticks does not account
+        // for leap seconds!
+        var now = DateTime.UtcNow;
+        // We could get tickOfDay directly from now.TimeOfDay.Ticks but to
+        // build TimeOfDay, DateTime does exactly what we do below.
+        ulong ticksSinceZero = (ulong)now.Ticks;
+        ulong daysSinceZero = TemporalArithmetic.DivideByTicksPerDay(ticksSinceZero, out ulong tickOfDay);
+        ulong millisecondOfDay = tickOfDay / TicksPerMillisecond;
+
+        // NB: the casts should always succeed.
+        var dayNumber = new DayNumber((int)daysSinceZero);
+        var timeOfDay = TimeOfDay.FromMillisecondOfDay((int)millisecondOfDay);
+
+        return new Moment(dayNumber, timeOfDay);
+    }
+
+    /// <inheritdoc/>
+    [Pure]
+    public DayNumber Today()
+    {
+        // NB: the cast should always succeed.
+        int daysSinceZero = (int)TemporalArithmetic.DivideByTicksPerDay(DateTime.UtcNow.Ticks);
+        return new DayNumber(daysSinceZero);
+    }
+}
