2dbbbf2e41a2479be20553cbc7b43a779dff06e9
Chtoucas <policromia@gmail.com>
2025-01-18 11:59:30 +0100

    Test IMonth

---
+++ src/Calendrie.Testing/Faux/FauxTropicaliaMonth.cs
@@ -0,0 +1,77 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Testing.Faux;
+
+using Calendrie;
+using Calendrie.Core.Schemas;
+using Calendrie.Core.Utilities;
+using Calendrie.Hemerology;
+using Calendrie.Systems;
+
+// The sole purpose of this type is to test IMonth.CountElapsedMonthsInYear().
+
+public readonly struct FauxTropicaliaMonth : IMonth, IEquatable<FauxTropicaliaMonth>
+{
+    public FauxTropicaliaMonth(int year, int month)
+    {
+        if (year < StandardScope.MinYear || year > StandardScope.MaxYear)
+            ThrowHelpers.ThrowYearOutOfRange(year);
+        if (month < 1 || month > TropicalistaSchema.MonthsInYear)
+            ThrowHelpers.ThrowMonthOutOfRange(month);
+
+        MonthsSinceEpoch = TropicalistaSchema.MonthsInYear * (year - 1) + month - 1;
+    }
+
+    public static TropicaliaCalendar Calendar => TropicaliaCalendar.Instance;
+    static Calendar IMonth.Calendar => Calendar;
+
+    public int MonthsSinceEpoch { get; }
+
+    public Ord CenturyOfEra => Ord.FromInt32(Century);
+    public int Century => YearNumbering.GetCentury(Year);
+    public Ord YearOfEra => Ord.FromInt32(Year);
+    public int YearOfCentury => YearNumbering.GetYearOfCentury(Year);
+    public int Year => 1 + MonthsSinceEpoch / TropicalistaSchema.MonthsInYear;
+
+    public int Month { get { var (_, m) = this; return m; } }
+
+    bool IMonth.IsIntercalary => false;
+
+    public void Deconstruct(out int year, out int month)
+    {
+        year = 1 + MathN.Divide(MonthsSinceEpoch, TropicalistaSchema.MonthsInYear, out int m0);
+        month = 1 + m0;
+    }
+
+    public int CountRemainingMonthsInYear() => TropicalistaSchema.MonthsInYear - Month;
+
+    public int CountElapsedDaysInYear()
+    {
+        var (y, m) = this;
+        return Calendar.Schema.CountDaysInYearBeforeMonth(y, m);
+    }
+
+    public int CountRemainingDaysInYear()
+    {
+        var (y, m) = this;
+        return Calendar.Schema.CountDaysInYearAfterMonth(y, m);
+    }
+
+    //
+    // IEquatable
+    //
+
+    public static bool operator ==(FauxTropicaliaMonth left, FauxTropicaliaMonth right) =>
+        left.MonthsSinceEpoch == right.MonthsSinceEpoch;
+
+    public static bool operator !=(FauxTropicaliaMonth left, FauxTropicaliaMonth right) =>
+        left.MonthsSinceEpoch != right.MonthsSinceEpoch;
+
+    public bool Equals(FauxTropicaliaMonth other) => MonthsSinceEpoch == other.MonthsSinceEpoch;
+
+    public override bool Equals([NotNullWhen(true)] object? obj) =>
+        obj is FauxTropicaliaMonth month && Equals(month);
+
+    public override int GetHashCode() => MonthsSinceEpoch;
+}
---
+++ src/Calendrie.Tests/Tests/Hemerology/InterfacesFacts.fs
@@ -0,0 +1,38 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+module Calendrie.Tests.Hemerology.InterfacesFacts
+
+#nowarn 3536 // Conversion to IMonth
+
+open Calendrie.Hemerology
+open Calendrie.Testing
+open Calendrie.Testing.Data
+open Calendrie.Testing.Data.Bounded
+open Calendrie.Testing.Facts.Hemerology
+open Calendrie.Testing.Faux
+
+open Xunit
+
+module DateInterfaces =
+    [<Sealed>]
+    [<TestExcludeFrom(TestExcludeFrom.Regular)>]
+    type DateFacts() =
+        inherit IDateFacts<FauxGregorianDate, StandardGregorianDataSet>()
+
+        // TDate.FromDayNumber() throws an OverflowException here but only because
+        // the base test uses the explicit implementation of FromDayNumber().
+        override x.FromDayNumber_InvalidDayNumber () =
+            x.DomainTester.TestInvalidDayNumber(FauxGregorianDate.FromDayNumber);
+
+module MonthInterface =
+    let calendarDataSet = StandardTropicaliaDataSet.Instance
+
+    let monthInfoData = calendarDataSet.MonthInfoData
+
+    [<Theory; MemberData(nameof(monthInfoData))>]
+    let ``CountElapsedMonthsInYear()`` (info: MonthInfo) =
+        let y, m = info.Yemo.Deconstruct()
+        let month : IMonth = new FauxTropicaliaMonth(y, m)
+
+        month.CountElapsedMonthsInYear() === m - 1
