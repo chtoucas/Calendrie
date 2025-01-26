3103e04d31bdaa622da0918721b0ef772fbb4433
Chtoucas <policromia@gmail.com>
2025-01-26 11:37:39 +0100

    Simple tests for the clock

---
+++ src/Calendrie.Testing/Faux/FauxClock.cs
@@ -0,0 +1,19 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+namespace Calendrie.Testing.Faux;
+
+using Calendrie.Horology;
+
+public sealed class FauxClock : IClock
+{
+    private readonly int _daysSinceZero;
+
+    public FauxClock(int daysSinceZero)
+    {
+        _daysSinceZero = daysSinceZero;
+    }
+
+    [Pure]
+    public DayNumber Today() => new(_daysSinceZero);
+}
---
+++ src/Calendrie.Tests/Tests/Horology/ClockTests.fs
@@ -0,0 +1,53 @@
+﻿// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) Tran Ngoc Bich. All rights reserved.
+
+module Calendrie.Tests.Horology.ClockTests
+
+open System
+
+open Calendrie
+open Calendrie.Systems
+open Calendrie.Testing
+open Calendrie.Testing.Faux
+
+open Xunit
+
+// TODO(fact): Property-based tests. Waiting for migration to the new version of FsCheck
+let private daysSinceZero = 123_456
+
+[<Fact>]
+let ``FauxClock:Today() sanity checks`` () =
+    let clock = FauxClock(daysSinceZero)
+    let dayNumber = DayNumber.Zero + daysSinceZero
+    // Act & Assert
+    clock.Today() === dayNumber
+
+module TropicaliaClock =
+    [<Fact>]
+    let ``Constructor throws when clock is null`` () =
+        nullExn "clock" (fun () -> new TropicaliaClock(null))
+
+    [<Fact>]
+    let ``GetCurrentXXX()`` () =
+        let clock = new TropicaliaClock(FauxClock(daysSinceZero))
+        let dayNumber = DayNumber.Zero + daysSinceZero
+        let date = TropicaliaDate.FromAbsoluteDate(dayNumber)
+        // Act & Assert
+        clock.GetCurrentDate()  === date
+        clock.GetCurrentMonth() === TropicaliaMonth.FromDate(date)
+        clock.GetCurrentYear()  === TropicaliaYear.FromDate(date)
+
+module PaxClock =
+    [<Fact>]
+    let ``Constructor throws when clock is null`` () =
+        nullExn "clock" (fun () -> new PaxClock(null))
+
+    [<Fact>]
+    let ``GetCurrentXXX()`` () =
+        let clock = new PaxClock(FauxClock(daysSinceZero))
+        let dayNumber = DayNumber.Zero + daysSinceZero
+        let date = PaxDate.FromAbsoluteDate(dayNumber)
+        // Act & Assert
+        clock.GetCurrentDate()  === date
+        clock.GetCurrentMonth() === PaxMonth.FromDate(date)
+        clock.GetCurrentYear()  === PaxYear.FromDate(date)
