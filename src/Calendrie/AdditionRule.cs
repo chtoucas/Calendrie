6cd93b633e011c662256afc009a72dc37e6bdc65
Chtoucas <policromia@gmail.com>
2025-01-28 11:14:10 +0100

    Update AdditionRule.cs

--- src/Calendrie/AdditionRule.cs
+++ src/Calendrie/AdditionRule.cs
@@ -3,11 +3,48 @@
 
 namespace Calendrie;
 
+// TODO(code): value names
+
 #region Developer Notes
 
-// Initialement, on proposait aussi une valeur Overflow, mais je pense que c'est
-// une mauvaise idée car cela reviendrait à utiliser une exception pour une
-// situation non-exceptionnelle.
+// Ajouter des années
+// ------------------
+//
+// Règle : quand une addition naïve donnerait un résultat invalide, on
+// sélectionne le dernier jour valide précédent.
+// Cette règle est simple à retenir mais ne donne pas nécessairement le
+// résultat le plus naturel, tout particulièrement quand le calendrier n'est pas
+// régulier.
+//
+// ## Calendrier grégorien régulier
+// Seul le jour intercalaire d'une année bissextile pose problème.
+// 29/02 +/- 1 année = 28/02 ; roundoff = 1
+//
+// ## Calendrier copte régulier (12 mois)
+// Seul le jour intercalaire d'une année bissextile pose problème.
+// 36/12 +/- 1 année = 35/12 ; roundoff = 1
+//
+// ## Calendrier copte régulier (13 mois)
+// Seul le jour intercalaire d'une année bissextile pose problème.
+// 06/13 +/- 1 année = 05/13 ; roundoff = 1
+//
+// ## Calendrier pax __très__ régulier (13 mois)
+// Année bissextile vers année ordinaire :
+// 01/13 +/- 1 année = 01/13
+// 07/13 +/- 1 année = 07/13
+// 01/14 +/- 1 année = 28/13 ; roundoff = 1     -20 ? 08/13 (0) - 08/13 (exact)
+// 21/14 +/- 1 année = 28/13 ; roundoff = 21      0 ? 28/13 (0) - 28/13 (exact)
+// 22/14 +/- 1 année = 28/13 ; roundoff = 22      1 ? 28/13 (1) - 01/14 (exact)
+// 28/14 +/- 1 année = 28/13 ; roundoff = 28      7 ? 28/13 (7) - 07/14 (exact)
+//
+// Année ordinaire vers année bissextile :
+// 01/13 +/- 1 année = 01/13
+// 07/13 +/- 1 année = 07/13
+// 08/13 +/- 1 année = 07/13 ; roundoff = 1
+// 28/13 +/- 1 année = 07/13 ; roundoff = 21
+//
+// Ajouter des mois
+// ----------------
 //
 // Exemples (calendriers grégorien et julien) quand le résultat d'une opération
 // arithmétique n'est pas exact (roundoff > 0).
@@ -45,6 +82,13 @@ namespace Calendrie;
 // * 30/5/2015 + 9 mois = 2/3/2016 (décalage = +2)
 // * 31/5/2015 - 3 mois = 3/3/2015 (décalage = +3)
 // * 30/5/2015 - 3 mois = 2/3/2015 (décalage = +2)
+//
+// Notes
+// -----
+//
+// Initialement, on proposait aussi une valeur Overflow, mais je pense que c'est
+// une mauvaise idée car cela reviendrait à utiliser une exception pour une
+// situation non-exceptionnelle.
 
 #endregion
 
