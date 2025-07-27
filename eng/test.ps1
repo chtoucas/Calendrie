2baf8c10ebe9a887beba4997953732e262003a22
Chtoucas <policromia@gmail.com>
2025-07-27 08:34:05 +0200

    Update dependencies

--- eng/test.ps1
+++ eng/test.ps1
@@ -51,7 +51,6 @@ custom filters.
 Examples.
 > test.ps1 -NoBuild             # Default test plan (Debug), no build
 > test.ps1 regular -c Release   # Regular test plan (Release)
-> test.ps1 -NoBuild             # Default test plan (Debug), no build
 
 Commonly used parameters:
 > test.ps1 -NoBuild
